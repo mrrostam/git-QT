aee3aa425bbfae9c4da40137b7408ff23ecb568f
pouya <pooya.rostam@gmail.com>
2020-03-01 13:46:29 -0800

    buttons

--- tutor1.py
+++ tutor1.py
@@ -3,11 +3,33 @@ import sys
 from PyQt5 import QtWidgets, QtGui
 
 
+class StartNewGameBtn(QtWidgets.QPushButton):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setText("Start New Game")
+        self.move(20, 160)
+
+
+class QuitBtn(QtWidgets.QPushButton):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setText("Quit")
+        self.move(150, 160)
+        self.clicked.connect(QtWidgets.qApp.quit)
+        self.setToolTip("exit!")
+
+
 class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.setup()
+
+    def setup(self):
         self.setGeometry(200, 200, 400, 200)
-        self.setWindowTitle('Tutor1 - OOP')
+        self.setWindowTitle('Tutor1.2')
+        self.setToolTip("MainWindow.Tooltip")
+        self.new_button = StartNewGameBtn(self)
+        self.quit_button = QuitBtn(self)
         self.show()
 
 
