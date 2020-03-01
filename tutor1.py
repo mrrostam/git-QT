7df0cf41f052cab9566f799794d5b5551a752f8e
pouya <pooya.rostam@gmail.com>
2020-03-01 13:29:41 -0800

    +OOP

--- tutor1.py
+++ tutor1.py
@@ -2,8 +2,16 @@ import sys
 
 from PyQt5 import QtWidgets, QtGui
 
+
+class MyWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 400, 200)
+        self.setWindowTitle('Tutor1 - OOP')
+        self.show()
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    main_window = QtWidgets.QWidget()
-    main_window.show()
+    main_window = MyWindow()
     sys.exit(app.exec_())
